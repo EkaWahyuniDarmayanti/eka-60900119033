e05dcc78cdcc85a864fea360e830704b9b04f48f
LAPTOP-EK1RD5MC\ASUS-PC <60900119033@uin-alauddin.ac.id>
2021-10-26 12:34:39 +0800

    selamat siang

---
+++ eka-60900119033/resources/views/biod.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>nama             : eka wahyuni darmayanti</p>
+    <p>nim              : 60900119033</p>
+    <p>email            :60900119033@uin-alauddin.ac.id</p>
+    <p>kelas            : b</p>
+</body>
+</html>
